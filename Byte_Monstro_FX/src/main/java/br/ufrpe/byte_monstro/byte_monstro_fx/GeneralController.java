214e3ee4a255b8d0976f3a435a5d80febc72bf97
multi-megaman <84993974+multi-megaman@users.noreply.github.com>
2021-11-01 22:13:02 -0300

    Base da lista de usuário adicionada
    uma pequena demonstração de como vai ser o output no JavaFX da lista de usuários (nesse exemplo o programa vai mostrar uma lista na tela baseada em um arraylist de strings)

--- Byte_Monstro_FX/src/main/java/br/ufrpe/byte_monstro/byte_monstro_fx/GeneralController.java
+++ Byte_Monstro_FX/src/main/java/br/ufrpe/byte_monstro/byte_monstro_fx/GeneralController.java
@@ -1,18 +1,24 @@
 package br.ufrpe.byte_monstro.byte_monstro_fx;
 
 import com.dlsc.formsfx.model.structure.DateField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import br.ufrpe.byte_monstro.byte_monstro_fx.beans.*;
 
 import java.time.LocalDate;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GeneralController {
+
+    ArrayList<String> lista_teste = new ArrayList<String>();
 
     @FXML
     private TextField namePicker;
@@ -36,12 +42,30 @@ public class GeneralController {
     private DatePicker matriculaPicker;
 
     @FXML
+    private ListView<String> listaUsuarios;
+
+    private ObservableList<String> lista;
+
+    public void initialize(){
+        lista = FXCollections.observableArrayList();
+        genItems();
+        listaUsuarios.setItems(lista);
+
+        //registerEventHandlers();
+    }
+    private void genItems() {
+        for(int i=0;i<10;i++){
+            lista.add(new String("Aluno_"+Integer.toString(i)));
+        }
+    }
+
+    @FXML
     public void btnOkPressed(ActionEvent eventoacao) {
         int randomNum = ThreadLocalRandom.current().nextInt(1, 1000000);
 
         System.out.println("OK pressionado");
 
-        String nomeAluno = namePicker.getText();
+        /*String nomeAluno = namePicker.getText();
         int idadeAluno =Integer.parseInt(idadePicker.getText());
         char generoAluno = generoPicker.getText().charAt(0);
         double pesoAluno = Double.parseDouble(pesoPicker.getText());
@@ -50,7 +74,7 @@ public class GeneralController {
         LocalDate matriculaAluno = matriculaPicker.getValue();
 
 
-        System.out.println(nomeAluno +" "+ idadeAluno);
+        System.out.println(nomeAluno +" "+ idadeAluno);*/
 
         //AQUI SERIA A FUNÇÃO PADRÃO PARA COLOCAR UM NOVO USUÁRIO NA LISTA
         //Aluno alunoNovo = new Aluno(randomNum,nomeAluno,idadeAluno,generoAluno,pesoAluno,alturaAluno,gorduraAluno,matriculaAluno);
