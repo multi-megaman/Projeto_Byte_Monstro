e9b8b1885e474b7eefbb4983e3b99bad6be88d20
multi-megaman <84993974+multi-megaman@users.noreply.github.com>
2021-11-16 17:33:31 -0300

    Update EnumAcademias.java

--- Byte_Monstro_FX/src/main/java/br/ufrpe/byte_monstro/byte_monstro_fx/beans/EnumAcademias.java
+++ Byte_Monstro_FX/src/main/java/br/ufrpe/byte_monstro/byte_monstro_fx/beans/EnumAcademias.java
@@ -1,4 +1,4 @@
-package Byte_Monstro_FX.src.main.java.br.ufrpe.byte_monstro.byte_monstro_fx.beans;
+package br.ufrpe.byte_monstro.byte_monstro_fx.beans;
 
 public enum EnumAcademias {
     ACADEMIA1, ACADEMIA2, ACADEMIA3
